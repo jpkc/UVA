daabded0183d758b511eb55b712a20eb862cb22b
João Pedro Kerr Catunda <jpkc_deb@DebianCortex.Tensores>
2019-05-01 04:55:10 -0300

    Refactoring: English switched enum and comments. Added Function signatures

--- 11624_Fire/11624_Fire.cpp
+++ 11624_Fire/11624_Fire.cpp
@@ -1,3 +1,5 @@
+//UVA problem 11624 - Fire! Solution by jpkc
+
 #include <iostream>
 #include <utility>
 #include <deque>
@@ -7,14 +9,38 @@
 
 using namespace std;
 
-//TODO:	Migrar para C puro. Implementar com BFS. Prmeiro do fogo, depois do Joe. Joe só pode avançar se a posição dele estiver com t menor que a do fogo, onde t é o valor do vetor visited.
-//			Deve ser mais lenta que a iterativa pois precisa verificar todo o DFS do Fogo e não apenas o necessário
+//TODO:	Code optimization round. 
+//				1: Switch to C and manually alloc memory. Avoiding <vector> allocation should drastically enhance speed.
+//				2: Test a BFS approach. First fire's maze run and than Joe's. Joe can only jump to positions where his t is smaller than the fire's where t is the cell's visited vector value. Beautiful but slower because maps whole maze , not just what's necessary.
 
 enum JOE_STATUS {
-	MORTO		= 1,
-	CORRENDO	= 2,
-	SALVO		= 4
+	DEAD		= 1,
+	RUNNING	= 2,
+	SAFE		= 4
 };
+
+void MazePrint(vector <vector <char>> *maze, unsigned int r, unsigned int c);
+JOE_STATUS MazeMark(vector <vector <char> > *maze, deque<pair<int, int> > *casted_block, char block);
+int MazeRun();
+
+
+int main(int argc, char *argv[])
+{
+	unsigned int n;
+	cin >> n;
+	for(unsigned int k = 0; k < n; ++k) {
+		if(VISUAL_FEEDBACK) {
+			cout << k << endl;
+		}
+		int t = MazeRun();
+		if(t) {
+			cout << t << endl;
+		} else {
+			cout << "IMPOSSIBLE" << endl;
+		}
+	}
+	return 0;
+}
 
 void MazePrint(vector <vector <char>> *maze, unsigned int r, unsigned int c) {
 	for(unsigned j = 0; j < c; ++j) {
@@ -27,7 +53,7 @@ void MazePrint(vector <vector <char>> *maze, unsigned int r, unsigned int c) {
 
 JOE_STATUS MazeMark(vector <vector <char> > *maze, deque<pair<int, int> > *casted_block, char block) {
 	pair<int, int> block_pos, proxy;
-	JOE_STATUS status = MORTO;
+	JOE_STATUS status = DEAD;
 	block_pos = casted_block->front();
 	casted_block->pop_front();
 
@@ -37,18 +63,18 @@ JOE_STATUS MazeMark(vector <vector <char> > *maze, deque<pair<int, int> > *caste
 		if((*maze)[proxy.second][proxy.first] == '.') {
 			(*maze)[proxy.second][proxy.first] = block;
 			casted_block->push_back(proxy);
-			if(status == MORTO) {
-				status = CORRENDO;
+			if(status == DEAD) {
+				status = RUNNING;
 			}
 		} else if((*maze)[proxy.second][proxy.first] == 'J' && block == 'F') {
 			(*maze)[proxy.second][proxy.first] = block;
 			casted_block->push_back(proxy);
-			if(status == MORTO) {
-				status = CORRENDO;
+			if(status == DEAD) {
+				status = RUNNING;
 			}
 		}
 	} else {
-		status = SALVO;
+		status = SAFE;
 	}
 
 	proxy.first = block_pos.first + 1;
@@ -57,18 +83,18 @@ JOE_STATUS MazeMark(vector <vector <char> > *maze, deque<pair<int, int> > *caste
 		if((*maze)[proxy.second][proxy.first] == '.') {
 			(*maze)[proxy.second][proxy.first] = block;
 			casted_block->push_back(proxy);
-			if(status == MORTO) {
-				status = CORRENDO;
+			if(status == DEAD) {
+				status = RUNNING;
 			}
 		} else if((*maze)[proxy.second][proxy.first] == 'J' && block == 'F') {
 			(*maze)[proxy.second][proxy.first] = block;
 			casted_block->push_back(proxy);
-			if(status == MORTO) {
-				status = CORRENDO;
+			if(status == DEAD) {
+				status = RUNNING;
 			}
 		}
 	} else {
-		status = SALVO;
+		status = SAFE;
 	}
 
 	proxy.first = block_pos.first;
@@ -77,18 +103,18 @@ JOE_STATUS MazeMark(vector <vector <char> > *maze, deque<pair<int, int> > *caste
 		if((*maze)[proxy.second][proxy.first] == '.') {
 			(*maze)[proxy.second][proxy.first] = block;
 			casted_block->push_back(proxy);
-			if(status == MORTO) {
-				status = CORRENDO;
+			if(status == DEAD) {
+				status = RUNNING;
 			}
 		} else if((*maze)[proxy.second][proxy.first] == 'J' && block == 'F') {
 			(*maze)[proxy.second][proxy.first] = block;
 			casted_block->push_back(proxy);
-			if(status == MORTO) {
-				status = CORRENDO;
+			if(status == DEAD) {
+				status = RUNNING;
 			}
 		}
 	} else {
-		status = SALVO;
+		status = SAFE;
 	}
 
 	proxy.first = block_pos.first;
@@ -97,18 +123,18 @@ JOE_STATUS MazeMark(vector <vector <char> > *maze, deque<pair<int, int> > *caste
 		if((*maze)[proxy.second][proxy.first] == '.') {
 			(*maze)[proxy.second][proxy.first] = block;
 			casted_block->push_back(proxy);
-			if(status == MORTO) {
-				status = CORRENDO;
+			if(status == DEAD) {
+				status = RUNNING;
 			}
 		} else if((*maze)[proxy.second][proxy.first] == 'J' && block == 'F') {
 			(*maze)[proxy.second][proxy.first] = block;
 			casted_block->push_back(proxy);
-			if(status == MORTO) {
-				status = CORRENDO;
+			if(status == DEAD) {
+				status = RUNNING;
 			}
 		}
 	} else {
-		status = SALVO;
+		status = SAFE;
 	}
 	return status;
 }
@@ -148,10 +174,10 @@ int MazeRun() {
 		MazePrint(&maze, r, c);
 	}
 
-	int joe_status = CORRENDO;
+	int joe_status = RUNNING;
 	int time = 1;
-	for(; joe_status == CORRENDO; ++time) {
-		joe_status = MORTO;
+	for(; joe_status == RUNNING; ++time) {
+		joe_status = DEAD;
 		for(unsigned int l = casted_fire.size(); l > 0; --l) {
 			MazeMark(&maze, &casted_fire, 'F');
 		}
@@ -162,33 +188,15 @@ int MazeRun() {
 
 		for(unsigned int l = casted_joe.size(); l > 0; --l) {
 			int status = MazeMark(&maze, &casted_joe, 'J');
-			if(status == SALVO) {
+			if(status == SAFE) {
 				return time;
-			} else if(status == CORRENDO) {
-				joe_status = CORRENDO;
+			} else if(status == RUNNING) {
+				joe_status = RUNNING;
 			}
 		}
 		if(VISUAL_FEEDBACK) {
 			cout << endl;
 			MazePrint(&maze, r, c);
-		}
-	}
-	return 0;
-}
-
-int main(int argc, char *argv[])
-{
-	unsigned int n;
-	cin >> n;
-	for(unsigned int k = 0; k < n; ++k) {
-		if(VISUAL_FEEDBACK) {
-			cout << k << endl;
-		}
-		int t = MazeRun();
-		if(t) {
-			cout << t << endl;
-		} else {
-			cout << "IMPOSSIBLE" << endl;
 		}
 	}
 	return 0;
